81b69dbaa407c5bcd8f0917bc767c07de03b694d
TheFJcurve <sargunbhatti27@gmail.com>
2025-08-26 16:28:46 +0800

    initial commit

---
+++ BetterNotes/BetterNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterNotesApp.swift
+//  BetterNotes
+//
+//  Created by Sargun Singh Bhatti on 2025-08-26.
+//
+
+import SwiftUI
+
+@main
+struct BetterNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterNotes/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BetterNotes
+//
+//  Created by Sargun Singh Bhatti on 2025-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("This is now getting interesting!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
